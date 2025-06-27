b2a28a9fa3795eafca050f1a0dc721973bb5caf1
lomaster <artemiykastapro@gmail.com>
2025-06-27 13:58:22 +0300

    updt

--- scripts/sc.sh
+++ scripts/sc.sh
@@ -1,4 +1,7 @@
-DIR="$HOME/screenshots"
+#!/bin/bash
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+DIR="$HOME/screenshots/$YEAR/$MONTH"
 mkdir -p "$DIR"
 FILENAME="$DIR/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
 scrot -s "$FILENAME"
